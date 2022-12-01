5b98a6954839659a09be8670ac1fba9cfd1a1f11
Kaym Kassai <kassai.kaym@gmail.com>
2022-12-01 15:12:54 +0100

    update db url

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
 
-mongoose.connect("mongodb://localhost:27017/ynov-pwa")
+mongoose.connect("mongodb+srv://kaymkassai:kaymkassai@cluster0.yuamcp6.mongodb.net/?retryWrites=true&w=majority")
   .then(() => console.log('✅ Successfully connected to the database'))
   .catch((e) => console.log(`⛔️ Error during database connection ${e}`))
